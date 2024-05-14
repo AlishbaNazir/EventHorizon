5eae8b98970a0184599596150b8c19c927f1cb2b
MaAn-41 <f219219@cfd.nu.edu.pk>
2024-05-14 12:22:50 +0500

    Regex Update

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -10,10 +10,10 @@ function validatePassword() {
 
     var alertDiv = document.createElement("div");
     if (!re.test(password)) {
-        // alertDiv.className = "alert alert-danger";
-        // alertDiv.role = "alert";
-        // alertDiv.textContent = "Please choose a strong password!";
-        // document.body.insertBefore(alertDiv, document.body.firstChild);
+        alertDiv.className = "alert alert-danger";
+        alertDiv.role = "alert";
+        alertDiv.textContent = "Please choose a strong password!";
+        document.body.insertBefore(alertDiv, document.body.firstChild);
         return false;
     }
     return true;
@@ -36,5 +36,75 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById("submitBtn").disabled = true;
         }
     });
+
+    //phone no must be equal to 11 digits
+
+    document.getElementById("numberField").addEventListener("input", function() {
+        var phone = document.getElementById("numberField").value;
+        var re = /^\d{11}$/;
+        var existingAlerts = document.querySelectorAll(".alert");
+
+        // Remove existing alerts
+        existingAlerts.forEach(function(alert) {
+            alert.parentNode.removeChild(alert);
+        });
+
+        var alertDiv = document.createElement("div");
+        if (!re.test(phone)) {
+            alertDiv.className = "alert alert-danger";
+            alertDiv.role = "alert";
+            alertDiv.textContent = "Please enter a valid phone number!";
+            document.body.insertBefore(alertDiv, document.body.firstChild);
+            return false;
+        }
+        return true;
+    });
     
+    //username must contain alphabets not only digits
+    document.getElementById("username").addEventListener("input", function() {
+        var username = document.getElementById("username").value;
+        var re = /^[a-zA-Z]+$/;
+        var existingAlerts = document.querySelectorAll(".alert");
+
+        // Remove existing alerts
+        existingAlerts.forEach(function(alert) {
+            alert.parentNode.removeChild(alert);
+        });
+
+        var alertDiv = document.createElement("div");
+        if (!re.test(username)) {
+            alertDiv.className = "alert alert-danger";
+            alertDiv.role = "alert";
+            alertDiv.textContent = "Please enter a valid username!";
+            document.body.insertBefore(alertDiv, document.body.firstChild);
+            return false;
+        }
+        return true;
+    });
+
+    //email must contain the valid format
+
+    document.getElementById("emailField").addEventListener("input", function() {
+        var email = document.getElementById("emailField").value;
+        var re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        var existingAlerts = document.querySelectorAll(".alert");
+
+        // Remove existing alerts
+        existingAlerts.forEach(function(alert) {
+            alert.parentNode.removeChild(alert);
+        });
+
+        var alertDiv = document.createElement("div");
+        if (!re.test(email)) {
+            alertDiv.className = "alert alert-danger";
+            alertDiv.role = "alert";
+            alertDiv.textContent = "Please enter a valid email!";
+            document.body.insertBefore(alertDiv, document.body.firstChild);
+            return false;
+        }
+        return true;
+    });
+
+
+
 });
